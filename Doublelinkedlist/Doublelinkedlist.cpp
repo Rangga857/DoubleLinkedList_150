01c6d3fd6d72aa7402171b299c65722e954297cb
Rangga857 <azharrangga28@gmail.com>
2023-05-26 00:21:30 +0700

    membuat  void DoubleLinkedList::revtraverse

--- Doublelinkedlist/Doublelinkedlist.cpp
+++ Doublelinkedlist/Doublelinkedlist.cpp
@@ -108,10 +108,26 @@ void DoubleLinkedList::traverse() {
 		Node* currentNode = START;
 		while (currentNode != NULL) {
 			cout << currentNode->noMhs << " " << currentNode->name << endl;
-			currentNode = currentNode ->
+			currentNode = currentNode->next;
 		}
 	}
 
+}
+
+void DoubleLinkedList::revtraverse() {
+	if (ListEmpty()) {
+		cout << "\nList is empty" << endl;
+	}
+	else {
+		cout << "\nRecords in a]descending order of roll number are: " << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode = currentNode->next;
+
+		while (currentNode != NULL) {
+			currentNode = currentNode->prev;
+		}
+	}
 }
 
 int main() {
