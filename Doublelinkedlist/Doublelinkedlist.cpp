3ed1e35d3b1add3ef1199775548d1751a8a82330
Rangga857 <azharrangga28@gmail.com>
2023-05-25 15:39:50 +0700

    step satu yang menunjukan Node* newnode = new Node()

--- Doublelinkedlist/Doublelinkedlist.cpp
+++ Doublelinkedlist/Doublelinkedlist.cpp
@@ -31,6 +31,11 @@ DoubleLinkedList::DoubleLinkedList() {
 void DoubleLinkedList::addNode() {
 	int nim;
 	string nm;
+	cout << "\nEnter the roll number of the student : ";
+	cin >> nim;
+	cout << "\nEnter the name of the student : ";
+	cin >> nm;
+	Node* newNode = new Node(); //step 1
 
 }
 
